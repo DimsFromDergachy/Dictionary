8285de7ea1cfeae75729f660700bc9530f2be356
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2023-08-10 01:40:15 +0400

    Add HttpClient for wiktionary

--- Program.cs
+++ Program.cs
@@ -3,7 +3,19 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/{name:alpha}", (string name) => $"Hello {name}");
+
+var GetWord = async (string word) => {
+    HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri($"https://ru.wiktionary.org"),
+    };
+    var result = await httpClient.GetAsync($"/wiki/{word}");
+    return await result.Content.ReadAsStringAsync();
+};
+
+app.MapGet("/{word:alpha}", async (string word) => {
+    var result = await GetWord(word);
+    return $"Hello {result}";
+});
 
 app.Run();
-
